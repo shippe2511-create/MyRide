014bd97e7d15dd13a64dc9f638621c2c85d8dd2f
Athif Abdulla <athifabdulla@mac.lan>
2026-06-21 17:40:30 +0500

    Fix syntax error in home_screen.dart

--- customer_app/lib/screens/home_screen.dart
+++ customer_app/lib/screens/home_screen.dart
@@ -660,6 +660,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                   ),
                 );
               }).toList(),
+            ),
           ),
         ),
       ],
